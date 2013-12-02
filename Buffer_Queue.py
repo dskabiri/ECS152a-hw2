02de93a4b0e321e1b390a78f1a5b49017808eb32
Shawn Shojaie <shawn.shojaie@gmail.com>
2013-12-01 23:37:06 -0800

    Add sample output.

--- Buffer_Queue.py
+++ Buffer_Queue.py
@@ -1,11 +1,13 @@
 import simpy
 import random
 
+# global class to total packets
 class G:
-	DroppedPkts = 0
-	TotalPkts = 0
+	def __init__(self):
+		self.DroppedPkts = 0
+		self.TotalPkts = 0
 
-
+# this class maintains
 class Buffer_Queue(object):
 
 	def __init__(self, env, maxSize, G):
@@ -48,19 +50,66 @@ def service(env, Mu, BQ):
 			print("Error")
 
 
-MAX_SIZE = 10
-LAMBD = .99
 MU = 1
-
-print("Begin Simulation")
-# random.seed(10)
-env = simpy.Environment()
-G = G()
-BQ = Buffer_Queue(env, MAX_SIZE, G)
-
-env.process(service(env, MU, BQ))
-env.process(arrival(env, LAMBD, BQ))
+MAX_SIZE_DICT = [10, 50]
+LAMBDA_DICT = [0.2, 0.4, 0.6, 0.8, 0.9, 0.99]
+time = 0
+interval = 100000
 
 
-env.run(until=1000000)
-print("Total Packets: {} Dropped Packets: {} Pd = {}".format(G.TotalPkts, G.DroppedPkts, G.DroppedPkts/float(G.TotalPkts)))
+for max_size in MAX_SIZE_DICT:
+
+	for lambd in LAMBDA_DICT:
+
+		print("Begin Simulation")
+		env = simpy.Environment()
+		glob = G()
+		BQ = Buffer_Queue(env, max_size, glob)
+
+		env.process(service(env, MU, BQ))
+		env.process(arrival(env, lambd, BQ))
+
+		time += interval
+		env.run(until=time)
+		print("Lambda: {}  Buffer Size: {}".format(lambd, max_size))
+		print("Total Packets: {} Dropped Packets: {} Pd = {}".format(glob.TotalPkts, glob.DroppedPkts, glob.DroppedPkts/float(glob.TotalPkts)))
+
+# Sample Output
+
+# Begin Simulation
+# Lambda: 0.2  Buffer Size: 10
+# Total Packets: 19916 Dropped Packets: 0 Pd = 0.0
+# Begin Simulation
+# Lambda: 0.4  Buffer Size: 10
+# Total Packets: 79783 Dropped Packets: 0 Pd = 0.0
+# Begin Simulation
+# Lambda: 0.6  Buffer Size: 10
+# Total Packets: 180167 Dropped Packets: 9 Pd = 4.99536541098e-05
+# Begin Simulation
+# Lambda: 0.8  Buffer Size: 10
+# Total Packets: 319606 Dropped Packets: 877 Pd = 0.00274400355438
+# Begin Simulation
+# Lambda: 0.9  Buffer Size: 10
+# Total Packets: 450717 Dropped Packets: 6973 Pd = 0.0154709052465
+# Begin Simulation
+# Lambda: 0.99  Buffer Size: 10
+# Total Packets: 593424 Dropped Packets: 25683 Pd = 0.0432793415838
+# Begin Simulation
+# Lambda: 0.2  Buffer Size: 50
+# Total Packets: 139997 Dropped Packets: 0 Pd = 0.0
+# Begin Simulation
+# Lambda: 0.4  Buffer Size: 50
+# Total Packets: 321028 Dropped Packets: 0 Pd = 0.0
+# Begin Simulation
+# Lambda: 0.6  Buffer Size: 50
+# Total Packets: 540587 Dropped Packets: 0 Pd = 0.0
+# Begin Simulation
+# Lambda: 0.8  Buffer Size: 50
+# Total Packets: 799858 Dropped Packets: 0 Pd = 0.0
+# Begin Simulation
+# Lambda: 0.9  Buffer Size: 50
+# Total Packets: 989538 Dropped Packets: 0 Pd = 0.0
+# Begin Simulation
+# Lambda: 0.99  Buffer Size: 50
+# Total Packets: 1187216 Dropped Packets: 6547 Pd = 0.00551458201372
+# [Finished in 56.2s]
